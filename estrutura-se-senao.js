18ba305e69da469e0fa9a286ecc56e79ba090bab
marcosbarret <marcos.emanuel.barreto@gmail.com>
2020-11-10 05:41:13 -0300

    Primeiro commit do código

---
+++ estrutura-se-senao.js
@@ -0,0 +1,51 @@
+/*
+   valor01, valor02, resultado: real
+   operacao: caractere
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc...
+   //escreva("A proposta desse programa é calcular dois valores baseado na operação da sua escolha. ")
+   escreva("Digite o primeiro valor: ")
+   leia(valor01)
+   escreva("Digite a opração: Ex: + , - , * , /")
+   leia(operacao)
+   escreva("Digite o segundo valor: ")
+   leia(valor02)
+   
+   se operacao = "+" entao
+      resultado := valor01 + valor02
+   senao
+        se operacao = "-" entao
+           resultado := valor01 - valor02
+        senao
+             se operacao = "*" entao
+                resultado := valor01 * valor02
+             senao
+                  se operacao = "/" entao
+                     resultado := valor01 / valor02
+                  fimse
+             fimse
+        fimse
+   fimse
+   
+   escreva("O Resultado do calculo é: ", resultado)
+*/
+
+function acaoBotao(params)  {
+    var valor01, valor02, resultado, operacao
+    valor01 = prompt("Digite o primeiro valor: ")
+    operacao = prompt("Digite a opração: Ex: + , - , * , /")
+    valor02 = prompt("Digite o segundo valor: ")
+
+    if ( operacao == "+" ){
+        resultado = parseInt( valor01 ) + parseInt( valor02 )
+    }else if(operacao == "-"){
+        resultado = parseInt( valor01 ) - parseInt( valor02 )
+    }else if(operacao == "*"){
+        resultado = parseInt( valor01 ) * parseInt( valor02 )
+    }else if(operacao == "/"){
+        resultado = parseInt( valor01 ) / parseInt( valor02 )
+    }
+ 
+    document.getElementById("paragrafo").innerText = resultado
+}
---
+++ input-output.js
@@ -0,0 +1,5 @@
+var nome, idade;
+nome = prompt("Digite o seu nome:")
+idade = prompt("Digite a sua nota:")
+alert("O nome digitado foi: " + nome)
+alert("A nota digitada foi: " + idade)
